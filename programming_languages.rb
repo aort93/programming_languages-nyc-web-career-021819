da89b9dcf215d602061ab64909fcd8e9c458481e
Alonso Ortega <alonsojortega@gmail.com>
2019-02-07 19:30:52 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,13 +3,16 @@ def reformat_languages(languages)
 
   languages.each do |style, lang_hash|
     lang_hash.each do |language, attribute_hash|
-      attribute_hash.each do ||
+      attribute_hash.each do |attribute, val|
         if new_hash[language] === nil
           new_hash[language] = {}
         end
 
         new_hash[language][:style] ||= []
         new_hash[language][:style].push(style)
+        if new_hash[language][attribute] === nil
+          new_hash[language][attribute] = val
+        end
       end
     end
   end
